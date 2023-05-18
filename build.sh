0411bb90ebc4289543970ba1a43b24d3db9acb1a
Codelab-Solux <codelabsolux@gmail.com>
2023-05-18 20:14:47 +0000

    pip eror patch

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # /!/bin/bash
 # Build the project
 echo 'Building the project ../..'
-python3.9 pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo 'Collecting static files ../..'
 python3.9 manage.py collectstatic --noinput --clear
