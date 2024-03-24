4226063bbde81025a23f778b9cd34755c11c8cf9
Codelab-Solux <codelabsolux@gmail.com>
2024-03-24 20:47:48 +0000

    csrf_trusted_origin patch

--- dermepro/settings.py
+++ dermepro/settings.py
@@ -37,6 +37,9 @@ CORS_ALLOWED_ORIGINS = [
     # Add any other allowed origins as needed
 ]
 
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/',
+                        'https://dermepro.onrender.com']
+
 
 # Application definition
 
